e6c4d5787579239be3f9c83d1543fd8bab5d8c2e
Mary62442 <maria.bu62442@gmail.com>
2020-01-22 21:49:46 +0100

    Implemented native stress test

--- stresstest.js
+++ stresstest.js
@@ -1,12 +1,35 @@
-var request = require('request');
 
+const request = require('request');
 
-    const stress = () =>{
-        request('http://localhost//',  (error, response, body) => {
+const requestData = {
+    scoredata: {
+        name :"Delfina la gallina",
+        city:"Montemanno Hen House",
+        age: 2,
+        score : 12,
+        telephone : "3476543212"
+       }
+};
+
+const iterations = 25001;
+let counter = 0;
+    const stress = () => {      
        
-        stress();
-    });
+        request({
+            url: "http://localhost:/setscoreinlist",
+            method: "POST",
+            json: requestData
+        }, (error, response, body) => {
+       
+           console.log(body);
+           counter += 1;
+           if(counter === iterations) return;
+           stress();
+
+        });  
     }
+
+
     stress();
    
 
